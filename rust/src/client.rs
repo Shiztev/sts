fb57fa5780a47ff945a957fe99b0943800dde49f
Stevie Alvarez <stevie.6strings@gmail.com>
2022-05-08 14:59:02 -0400

    init run stub

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -1,8 +1,17 @@
+/// Starts and manages both an SSH and a SFTP connection, running user commands.
+
+
+/// Start an SSH and SFTP connection, and loop while executing user commands.
+pub fn run() {
+  
+}
+
+/// SSH commands
 mod ssh {
 
 }
 
-
+/// SFTP commands
 mod sftp {
-  
+
 }
