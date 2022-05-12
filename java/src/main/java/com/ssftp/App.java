1365714b70fe84dfca23dd60694e8e422e994472
Stevie Alvarez <stevie.6strings@gmail.com>
2022-05-12 17:12:21 -0400

    beginning project

--- java/src/main/java/com/ssftp/App.java
+++ java/src/main/java/com/ssftp/App.java
@@ -1,13 +1,13 @@
 package com.ssftp;
 
 /**
- * Hello world!
- *
+ * Combine SSH and SFTP utility to easily navigate a system and manage files.
+ * 
+ * @author Stevie Alvarez
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main( String[] args ) {
+    // parse args
+    // if port provided, call correct SSFTP constructor
+  }
 }
