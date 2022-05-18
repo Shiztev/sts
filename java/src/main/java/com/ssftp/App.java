b873ee4bebfe6cb47647caea1ba7967b88e302e5
Stevie Alvarez <stevie.6strings@gmail.com>
2022-05-18 13:43:50 -0400

    added default args

--- java/src/main/java/com/ssftp/App.java
+++ java/src/main/java/com/ssftp/App.java
@@ -1,12 +1,16 @@
 package com.ssftp;
 
+import java.io.IOException;
+
 /**
  * Combine SSH and SFTP utility to easily navigate a system and manage files.
  * 
  * @author Stevie Alvarez
  */
 public class App {
-  public static void main( String[] args ) {
+  public static void main( String[] args ) throws IOException {
+    String username = "test";
+    String host = "this.place.dne.com";
     // TODO: parse args <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     // if port provided, call correct SSFTP constructor
     
