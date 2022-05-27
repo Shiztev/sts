56102218d178abb0e2f2c27d3bb487cbcdf19166
Stevie Alvarez <stevie.6strings@gmail.com>
2022-05-26 18:07:44 -0700

    verify args provided

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ use std::env;
 
 fn main() {
   let args:Vec<String> = env::args().collect();
+  
+  if args.len() != 1 {
+    panic!("usage: ssftp <username>@<ip/domain name/...>");
+  }
+
   client::run(args);
 }
