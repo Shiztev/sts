b5dc5d28d6ba85767f0905b56da7a71c71ed6c52
Stevie Alvarez <stevie.6strings@gmail.com>
2022-07-24 16:39:07 -0700

    run ssftp command prompt after initializing ssh2 session

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::env;
 fn main() {
   let args:Vec<String> = env::args().collect();
   
-  if args.len() != 1 {
+  if args.len() != 2 {
     panic!("usage: ssftp <username>@<ip/domain name/...>");
 
   } else {
-    let mut ssftp: Ssftp = Ssftp::new(&args[0]);
+    let mut ssftp: Ssftp = Ssftp::new(&args[1]);
     ssftp.ssh_init();
+    ssftp.run();
   }
 }
