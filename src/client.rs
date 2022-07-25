8dc2b9f7f329dd39150a84c0d2f79d7b4f6f8da9
Stevie Alvarez <stevie.6strings@gmail.com>
2022-07-24 23:03:49 -0700

    implement run_cmd, add documentation

--- src/client.rs
+++ src/client.rs
@@ -15,8 +15,9 @@ pub struct Ssftp {
 impl Ssftp {
   /// Initialize an Ssftp instance from the given username and address.
   pub fn new(args: &String) -> Ssftp {
+    // Distingush and setup address and username
     let p: Vec<&str> = args.split("@").collect();
-    let mut address = p[1].to_string();
+    let mut address: String = p[1].to_string();
     address.push_str(":22");
 
     let ssftp: Ssftp = Ssftp {
@@ -33,14 +34,14 @@ impl Ssftp {
 
   /// Start an ssh connection via tcp to the instances address. Prompts for a password.
   pub fn ssh_init(&mut self) {
+    // Establish tcp connection
     println!("establishing {}'s connection at {}...", self.username, self.addr);
-    let tcp_result = TcpStream::connect(self.addr.as_str());
-
-    let tcp = match tcp_result {
+    let tcp: TcpStream = match TcpStream::connect(self.addr.as_str()) {
       Ok(t) => t,
       Err(e) => panic!("Problem establishing connection: {}", e),
     };
 
+    // Set up session and authenticate
     self.sess.set_tcp_stream(tcp);
     self.sess.handshake().unwrap();
     // TODO: determine if a password is needed(?)
@@ -53,33 +54,16 @@ impl Ssftp {
 
   /// Prompts user for input and prints server response.
   pub fn run(self) {
-    let mut buf:String = String::new();
-    let mut channel: ssh2::Channel;
+    let mut cmd:String = String::new();
     let mut exit_code: i32;
 
-    while buf != "exit" {
+    while cmd != "exit" {
       // Read input
-      buf.clear();
-      stdin().read_line(&mut buf).expect("Problem reading user input");
+      cmd.clear();
+      stdin().read_line(&mut cmd).expect("Problem reading user input");
 
-      // Create channel
-      match self.sess.channel_session() {
-        Ok(c) => channel = c,
-        Err(e) => panic!("Probelm creating channel: {}", e),
-      }
-
-      // Execute command
-      channel.exec(&buf).expect("Problem executing command");
-      channel.read_to_string(&mut buf).expect("Problem reading server response");
-      println!("{}", buf);
-
-      // Cleanup and prep for next command
-      channel.wait_close().expect("Problem waiting on server result");
-      match channel.exit_status() {
-        Ok(n) => exit_code = n,
-        Err(e) => panic!("Problem getting exit status: {}", e)
-      }
-
+      // Execute command and print output
+      exit_code = self.run_cmd(&cmd);
       if exit_code != 0 {
         println!("Program ended with exit code {}", exit_code);
       }
@@ -89,8 +73,31 @@ impl Ssftp {
     }
   }
 
-  ///// Runs the provided command.
-  //fn run_cmd(cmd: String) {
+  /// Runs the provided command.
+  fn run_cmd(&self, cmd: &String) -> i32 {
+    let mut channel: ssh2::Channel;
+    let mut output: String = String::new();
+    let exit_code: i32;
 
-  //}
+    // Create channel
+    match self.sess.channel_session() {
+      Ok(c) => channel = c,
+      Err(e) => panic!("Probelm creating channel: {}", e),
+    }
+
+    // Execute command
+    channel.exec(&cmd).expect("Problem executing command");
+    channel.read_to_string(&mut output).expect("Problem reading server response");
+    println!("{}", output);
+
+    // Cleanup and prep for next command
+    channel.wait_close().expect("Problem waiting on server result");
+    match channel.exit_status() {
+      Ok(n) => exit_code = n,
+      Err(e) => panic!("Problem getting exit status: {}", e)
+    }
+
+    // TODO: update self.path
+    exit_code
+  }
 }
